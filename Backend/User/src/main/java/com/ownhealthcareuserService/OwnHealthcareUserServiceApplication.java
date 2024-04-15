8b9db89d8beaab2f74f4dd7c6f7589a686be203c
ranjithkumarYsi <ranjith.kumar@youngsoft.in>
2024-04-15 11:49:50 +0530

    User Service added

---
+++ Backend/User/src/main/java/com/ownhealthcareuserService/OwnHealthcareUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ownhealthcareuserService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnHealthcareUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwnHealthcareUserServiceApplication.class, args);
+	}
+
+}
